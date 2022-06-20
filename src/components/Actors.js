e8e851da4abe6174339a48971bd4528c36eadc78
Lyn <lynfinman@gmail.com>
2022-06-20 16:25:31 -0400

    made required updates

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,7 +2,28 @@ import React from "react";
 import { actors } from "../data";
 
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
+  const renderActors = () => {
+    return actors.map((actor) => {
+      return (
+        <div key={actor.name}>
+          <h2>Name: {actor.name}</h2>
+
+          <h3>Movies:</h3>
+          <ul>
+            {actor.movies.map((movie) => {
+              return <li key={movie}>{movie}</li>;
+            })}
+          </ul>
+        </div>
+      );
+    });
+  };
+  return (
+    <div>
+      <h1>Actors Page</h1>
+      {renderActors()}
+    </div>
+  );
 }
 
 export default Actors;
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,28 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  const renderMovies = () => {
+    return movies.map((movie) => {
+      return (
+        <div key={movie.title}>
+          <h2>Name: {movie.title}</h2>
+          <h3>Time: {movie.time}</h3>
+          <h3>Genres:</h3>
+          <ul>
+            {movie.genres.map((genre) => {
+              return <li key={genre}>{genre}</li>;
+            })}
+          </ul>
+        </div>
+      );
+    });
+  };
+  return (
+    <div>
+      <h1>Movies Page</h1>
+      {renderMovies()}
+    </div>
+  );
 }
 
 export default Movies;
